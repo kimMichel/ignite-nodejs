3089f7eeb8ece9ff5ce399c748622d66cd2f9a48
Michel Kim <kim98.michel@gmail.com>
2023-08-24 23:28:54 -0300

    saving users on memory (header)

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,13 +1,22 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("List of users");
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "Michel Kim",
+      email: "michelkim@example.com",
+    });
     return res.end("User created.");
   }
 
