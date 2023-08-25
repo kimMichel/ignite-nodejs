2350ba62a4696c13d4f4ec497c6a47bb252623d5
Michel Kim <kim98.michel@gmail.com>
2023-08-24 23:16:17 -0300

    http methods

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,16 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("List of users");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("User created.");
+  }
+
   return res.end("Hello Kim");
 });
 
