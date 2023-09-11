66ff6345088da51f0d8ac2c287b4969e7e6e05d1
Michel Kim <kim98.michel@gmail.com>
2023-09-11 00:38:55 -0300

    doing queries with knex

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { randomUUID } from 'crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Test Transaction',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app
