1fc7da169cc98ccfd6757b4d13f54c357619ee6e
Michel Kim <kim98.michel@gmail.com>
2023-09-12 22:11:04 -0300

    add example test

---
+++ 02-api-rest-nodejs/test/example.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+
+test('o usuario consegue criar uma nova transacao', () => {
+  const responseStatusCode = 201
+
+  expect(responseStatusCode).toEqual(201)
+})
