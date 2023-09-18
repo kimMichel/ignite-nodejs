095c7760292a67db98b1f336fc5b3027a6958d55
Michel Kim <kim98.michel@gmail.com>
2023-09-18 00:05:26 -0300

    create register service

---
+++ 03-api-solid/src/services/register.ts
@@ -0,0 +1,34 @@
+import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma'
+
+interface RegisterServiceRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerService({
+  name,
+  email,
+  password,
+}: RegisterServiceRequest) {
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('Email already exists.')
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+}
