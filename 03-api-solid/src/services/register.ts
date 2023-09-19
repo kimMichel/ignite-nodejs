9140b714b2ffb4d30ba5dd226fe6d06016902c06
Michel Kim <kim98.michel@gmail.com>
2023-09-18 23:45:55 -0300

    dependency inversion principles

--- 03-api-solid/src/services/register.ts
+++ 03-api-solid/src/services/register.ts
@@ -1,6 +1,5 @@
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 interface RegisterServiceRequest {
   name: string
@@ -8,28 +7,30 @@ interface RegisterServiceRequest {
   password: string
 }
 
-export async function registerService({
-  name,
-  email,
-  password,
-}: RegisterServiceRequest) {
-  const password_hash = await hash(password, 6)
+// SOLID
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+// D - Dependency Inversion Principle
+
+export class RegisterService {
+  constructor(private usersRepository: any) { }
+
+  async execute({ name, email, password }: RegisterServiceRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('Email already exists.')
+    }
+
+    await this.usersRepository.create({
+      name,
       email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('Email already exists.')
+      password_hash,
+    })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
