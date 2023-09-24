535f54b77af537388bb6389ea9c9fdafb012bf07
Michel Kim <kim98.michel@gmail.com>
2023-09-24 17:40:22 -0300

    add get user metrics service

---
+++ 03-api-solid/src/services/get-user-metrics.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface GetUserMetricsServiceRequest {
+  userId: string
+}
+
+interface GetUserMetricsServiceResponse {
+  checkInsCount: number
+}
+
+export class GetUserMetricsService {
+  constructor(
+    private checkInsRepository: CheckInsRepository
+  ) { }
+
+  async execute({
+    userId,
+  }: GetUserMetricsServiceRequest): Promise<GetUserMetricsServiceResponse> {
+    const checkInsCount = await this.checkInsRepository.countByUserId(userId)
+
+    return {
+      checkInsCount,
+    }
+  }
+}
