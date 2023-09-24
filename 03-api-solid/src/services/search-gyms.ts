9783f60181692104fbcb62b54ed0f586ef437124
Michel Kim <kim98.michel@gmail.com>
2023-09-24 18:00:12 -0300

    add search gyms by name service

---
+++ 03-api-solid/src/services/search-gyms.ts
@@ -0,0 +1,27 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '@/repositories/gyms-repository'
+
+interface SearchGymsServiceRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsServiceResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsService {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private gymsRepository: GymsRepository) { }
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsServiceRequest): Promise<SearchGymsServiceResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+
+    return {
+      gyms,
+    }
+  }
+}
