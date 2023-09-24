3f1da1d3d9abb20fbd1bf71399a85a467c5e1c30
Michel Kim <kim98.michel@gmail.com>
2023-09-24 17:26:29 -0300

    add check-in history service with pagination

---
+++ 03-api-solid/src/services/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface FetchUserCheckInsHistoryServiceRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryServiceResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryService {
+  constructor(
+    private checkInsRepository: CheckInsRepository
+  ) { }
+
+  async execute({
+    userId,
+    page
+  }: FetchUserCheckInsHistoryServiceRequest): Promise<FetchUserCheckInsHistoryServiceResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(userId, page)
+
+    return {
+      checkIns,
+    }
+  }
+}
