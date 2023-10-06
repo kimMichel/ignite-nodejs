918972091a44e379ff2d88092844fe17746ebad0
Michel Kim <kim98.michel@gmail.com>
2023-10-06 10:24:40 -0300

    create factory for other services

---
+++ 03-api-solid/src/services/factories/make-fetch-user-check-ins-history-service.ts
@@ -0,0 +1,8 @@
+import { FetchUserCheckInsHistoryService } from '../fetch-user-check-ins-history'
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+
+export function makeFetchUserCheckInsService() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const service = new FetchUserCheckInsHistoryService(checkInsRepository)
+  return service
+}
